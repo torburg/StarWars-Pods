ff95313ef81f978d2a4aa057c2e3c7ad723e40c5
torburg <torburg@yandex.ru>
2020-08-15 19:22:58 +0300

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Maksim Torburg on 15.08.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
